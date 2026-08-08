9d68edec8d1dcd092a442801dc27ac8e4a2bbdf4
Chamuditha-12 <chamudithamadushan54@gmail.com>
2026-08-08 16:48:59 +0530

    add SubmittedState allowing grade correction after submission

--- src/nexusenroll/patterns/state/SubmittedState.java
+++ src/nexusenroll/patterns/state/SubmittedState.java
@@ -1,5 +1,24 @@
 package nexusenroll.patterns.state;
 
-public class SubmittedState {
-    
+import nexusenroll.models.Grade;
+
+public class SubmittedState implements IGradeState {
+    @Override
+    public void submit(Grade grade) {
+        System.out.println("  Grade already submitted for " + grade.getStudentId() + "; no action taken.");
+    }
+
+    @Override
+    public void correct(Grade grade, char newLetter) {
+        // Assignment requirement: professor can still correct a submitted grade
+        // "without losing other submitted grades".
+        System.out.println("  Correcting SUBMITTED grade for " + grade.getStudentId()
+                + ": " + grade.getLetter() + " -> " + newLetter);
+        grade.setLetter(newLetter);
+    }
+
+    @Override
+    public String getStatus() {
+        return "Submitted";
+    }
 }
