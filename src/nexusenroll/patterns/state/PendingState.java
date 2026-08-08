637788e65038bd18a0d6857db836bf7ca3e32bd2
Chamuditha-12 <chamudithamadushan54@gmail.com>
2026-08-08 16:47:29 +0530

    add PendingState for the initial grade lifecycle stage

--- src/nexusenroll/patterns/state/PendingState.java
+++ src/nexusenroll/patterns/state/PendingState.java
@@ -1,5 +1,23 @@
 package nexusenroll.patterns.state;
 
-public class PendingState {
-    
+import nexusenroll.models.Grade;
+
+public class PendingState implements IGradeState {
+    @Override
+    public void submit(Grade grade) {
+        System.out.println("  Submitting grade " + grade.getLetter() + " for " + grade.getStudentId());
+        grade.setState(new SubmittedState());
+    }
+
+    @Override
+    public void correct(Grade grade, char newLetter) {
+        System.out.println("  Correcting PENDING grade for " + grade.getStudentId()
+                + ": " + grade.getLetter() + " -> " + newLetter);
+        grade.setLetter(newLetter);
+    }
+
+    @Override
+    public String getStatus() {
+        return "Pending";
+    }
 }
