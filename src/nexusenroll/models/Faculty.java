b67102694ee05ac6e80ca601bbf96f51cb0a6c94
Chamuditha-12 <chamudithamadushan54@gmail.com>
2026-08-08 16:58:49 +0530

    add Faculty model with assigned courses and department

--- src/nexusenroll/models/Faculty.java
+++ src/nexusenroll/models/Faculty.java
@@ -1,5 +1,34 @@
 package nexusenroll.models;
 
-public class Faculty {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class Faculty extends User {
+    private final List<Course> assignedCourses = new ArrayList<>();
+    private String department = "";
+
+    public Faculty(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public String getRole() {
+        return "Faculty";
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void assignCourse(Course course) {
+        assignedCourses.add(course);
+    }
+
+    public List<Course> getAssignedCourses() {
+        return assignedCourses;
+    }
 }
