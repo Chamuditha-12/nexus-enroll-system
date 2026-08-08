85f4ac09c261bc8bc4877b1b639b3742fec72e8c
Chamuditha-12 <chamudithamadushan54@gmail.com>
2026-08-08 16:57:49 +0530

    add Administrator model

--- src/nexusenroll/models/Administrator.java
+++ src/nexusenroll/models/Administrator.java
@@ -1,5 +1,12 @@
 package nexusenroll.models;
 
-public class Administrator {
-    
+public class Administrator extends User {
+    public Administrator(String id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    public String getRole() {
+        return "Administrator";
+    }
 }
